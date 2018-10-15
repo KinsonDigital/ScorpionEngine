7ee37f494fd4fc028cfe31cd4e45ec7d7d86a28f
Calvin Wilkinson <kinsondigital@gmail.com>
2018-10-15 06:14:11 -0400

    Add unit testing project for testing the ScorpionEngine project

---
+++ ScorpionEngine.Tests/MyTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ScorpionEngine.Tests
+{
+    public class MyTests
+    {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.Equal(3, 3);
+        }
+    }
+}
