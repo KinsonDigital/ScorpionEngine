80c07e3b9e251226ae7f172af4be6b7e1658a8f1
Calvin Wilkinson <kinsondigital@gmail.com>
2018-10-22 05:15:23 -0400

    Implement unit tests for GameText class

--- ScorpionEngine.Tests/GameTextTests.cs
+++ ScorpionEngine.Tests/GameTextTests.cs
@@ -93,6 +93,68 @@ namespace ScorpionEngine.Tests
             //Assert
             Assert.Equal(expected, actual);
         }
+
+
+        [Fact]
+        public void Height_WhenSettingValue_ProperlyReturnsInternalValue()
+        {
+            //Arrange
+            var mockInternalText = new Mock<IText>();
+            mockInternalText.Setup(m => m.Height).Returns(40);
+            var gameText = new GameText()
+            {
+                InternalText = mockInternalText.Object
+            };
+            var expected = 40;
+
+            //Act
+            var actual = gameText.Height;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Height_WhenSettingValue_ProperlySetsInternalValue()
+        {
+            //Arrange
+            var mockInternalText = new Mock<IText>();
+            mockInternalText.SetupProperty(m => m.Height, 0);
+            var gameText = new GameText()
+            {
+                InternalText = mockInternalText.Object
+            };
+            var expected = 10;
+
+            //Act
+            gameText.Height = 10;
+            var actual = gameText.Height;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Color_WhenSettingValue_ProperlySetsInternalValue()
+        {
+            //Arrange
+            var mockInternalText = new Mock<IText>();
+            mockInternalText.SetupProperty(m => m.Color, new byte[] { 11, 22, 33, 44 });
+            var gameText = new GameText()
+            {
+                InternalText = mockInternalText.Object
+            };
+            var expected = new GameColor(11, 22, 33, 44);
+
+            //Act
+            gameText.Color = new GameColor(11, 22, 33, 44);
+            var actual = gameText.Color;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
         #endregion
     }
 }
