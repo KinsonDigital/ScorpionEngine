7053786f142a04e506d7c05d13691e8b6e35c4c1
Calvin Wilkinson <kinsondigital@gmail.com>
2018-11-10 12:01:30 -0500

    Implement 100% unit test code coverage for Behavior class

---
+++ ScorpionEngine.Tests/Fakes/FakeBehavior.cs
@@ -0,0 +1,25 @@
+﻿using ScorpionEngine.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorpionEngine.Tests.Fakes
+{
+    public class FakeBehavior : Behavior
+    {
+        public bool UpdateActionInvoked { get; private set; }
+
+        public FakeBehavior(bool setupAction)
+        {
+            if (!setupAction)
+                return;
+
+            SetUpdateAction((engineTime) =>
+            {
+                UpdateActionInvoked = true;
+            });
+        }
+    }
+}
