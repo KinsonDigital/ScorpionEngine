25dab19c135b8433b181a9096cecd9a5161e5ef4
Calvin Wilkinson <kinsondigital@gmail.com>
2018-11-13 12:38:00 -0500

    Implement 100% unit test code coverage for FrameStackFinishedEventArgs class

---
+++ ScorpionEngine.Tests/Events/FrameStackFinishedEventArgsTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using ScorpionEngine.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorpionEngine.Tests.Events
+{
+    [TestFixture]
+    public class FrameStackFinishedEventArgsTests
+    {
+        #region Constructor Tests
+        [Test]
+        public void Ctor_WhenInvoking_SetTotalFramesRanProp()
+        {
+            //Arrange
+            var eventArgs = new FrameStackFinishedEventArgs(123);
+            var expected = 123;
+
+            //Act
+            var actual = eventArgs.TotalFramesRan;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+
+        #region Prop Tests
+        [Test]
+        public void TotalFramesRan_WhenGettingAndSettingValue_ReturnsCorrectValue()
+        {
+            //Arrange
+            var eventArgs = new FrameStackFinishedEventArgs(0);
+            var expected = 123;
+
+            //Act
+            eventArgs.TotalFramesRan = 123;
+            var actual = eventArgs.TotalFramesRan;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
