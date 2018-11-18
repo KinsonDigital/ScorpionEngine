74812cc3df4b29f14cb34e0734f77f62313f6f3f
Calvin Wilkinson <kinsondigital@gmail.com>
2018-11-18 08:04:01 -0500

    Implement 100% unit test code coverage for EventArgs classes

---
+++ ScorpionCore.Tests/OnUpdateEventArgsTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace ScorpionCore.Tests
+{
+    [TestFixture]
+    public class OnUpdateEventArgsTests
+    {
+        #region Constructor Tests
+        [Test]
+        public void Ctor_WhenInvoking_SetsRendererProp()
+        {
+            //Arrange
+            var mockRenderer = new Mock<IEngineTiming>();
+            var expected = mockRenderer.Object;
+            var eventArgs = new OnUpdateEventArgs(mockRenderer.Object);
+
+            //Act
+            var actual = eventArgs.EngineTime;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+
+        #region Prop Tests
+        [Test]
+        public void Renderer_WhenGettingAndSettingValue_ReturnsCorrectValue()
+        {
+            //Arrange
+            var mockRendererForCtor = new Mock<IEngineTiming>();
+            var eventArgs = new OnUpdateEventArgs(mockRendererForCtor.Object);
+            var mockRendererForProp = new Mock<IEngineTiming>();
+            var expected = mockRendererForProp.Object;
+
+            //Act
+            eventArgs.EngineTime = mockRendererForProp.Object;
+            var actual = eventArgs.EngineTime;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
