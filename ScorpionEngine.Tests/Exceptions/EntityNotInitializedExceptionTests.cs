c886987956de0d74efed288ce7d008ff1510438e
Calvin Wilkinson <kinsondigital@gmail.com>
2018-11-18 11:51:56 -0500

    Implement 100% unit test code coverage for EntityNotInitializedException class

---
+++ ScorpionEngine.Tests/Exceptions/EntityNotInitializedExceptionTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using ScorpionEngine.Entities;
+using ScorpionEngine.Exceptions;
+using ScorpionEngine.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScorpionEngine.Tests.Exceptions
+{
+    [TestFixture]
+    public class EntityNotInitializedExceptionTests
+    {
+        #region Constructor Tests
+        [Test]
+        public void Ctor_WhenInvokingWithNoParams_CorrectlySetsExceptionMessage()
+        {
+            //Arrange
+            var expected = $"{nameof(Entity)} not initialized.  Must be initialized before being added to a {nameof(PhysicsWorld)}";
+
+            //Act
+            var exception = new EntityNotInitializedException();
+            var actual = exception.Message;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void Ctor_WhenInvokingWithMessageParam_CorrectlySetsExceptionMessage()
+        {
+            //Arrange
+            var expected = "This is an exception message";
+
+            //Act
+            var exception = new EntityNotInitializedException("This is an exception message");
+            var actual = exception.Message;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
