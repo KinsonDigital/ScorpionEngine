1b105c0428c59a46f2b46d776419b003f7ae7ddb
Calvin Wilkinson <kinsondigital@gmail.com>
2018-12-11 06:42:37 -0500

    Get basic rendering working

---
+++ ParticleMaker/GraphicsEngine.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParticleMaker
+{
+    public class GraphicsEngine : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private IntPtr _windowsHandle;
+        private Control _window;
+
+
+        public GraphicsEngine(IntPtr windowHandle)
+        {
+            _windowsHandle = windowHandle;
+
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreparingDeviceSettings += _graphics_PreparingDeviceSettings;
+            Content.RootDirectory = "Content";
+        }
+
+
+        private void _graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
+        {
+            e.GraphicsDeviceInformation.PresentationParameters.DeviceWindowHandle = _windowsHandle;
+        }
+
+
+        protected override void Initialize()
+        {
+            Window.Position = new Point(-3000, Window.Position.Y);
+            _window = Control.FromHandle(Window.Handle);
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (_window.Visible)
+                _window.Hide();
+
+            base.Update(gameTime);
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(40, 40, 40, 255));
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.DrawRectangle(new Rectangle(100, 150, 100, 100), Color.CornflowerBlue);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+
+}
--- ParticleMaker/MainWindow.xaml.cs
+++ ParticleMaker/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using ThreadTimer = System.Threading.Timer;
 
 namespace ParticleMaker
 {
@@ -7,9 +10,42 @@ namespace ParticleMaker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GraphicsEngine _graphics;
+        private ThreadTimer _timer;
+        private bool _timerRan;
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            _timer = new ThreadTimer(TimerCallback, null, 0, 250);
+
+            base.OnInitialized(e);
+        }
+
+
+        private void TimerCallback(object state)
+        {
+            if (_timerRan)
+                return;
+
+            _timerRan = true;
+
+            Dispatcher.Invoke(() =>
+            {
+                if ((winFormsHost.Child as PictureBox).Handle != IntPtr.Zero)
+                {
+                    _timer?.Dispose();
+                    _timer = null;
+                    _graphics = new GraphicsEngine((winFormsHost.Child as PictureBox).Handle);
+                    _graphics.Run();
+                }
+            });
         }
     }
 }
