2f361b26823533a4702502b4b38d2eca8469bbd0
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-10 20:34:41 -0500

    ado-207 Add docs to the ColorItem class

--- ParticleMaker/ColorItem.cs
+++ ParticleMaker/ColorItem.cs
@@ -2,16 +2,33 @@
 
 namespace ParticleMaker
 {
+    /// <summary>
+    /// Represents a color with a unique ID.
+    /// </summary>
     public class ColorItem
     {
+        #region Props
+        /// <summary>
+        /// The unique ID of the color item.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// The color brush of the item.
+        /// </summary>
         public SolidColorBrush ColorBrush { get; set; }
+        #endregion
 
 
+        #region Public Methods
+        /// <summary>
+        /// Returns the string representation of the <see cref="ColorItem"/>.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"R:{ColorBrush.Color.R}, G:{ColorBrush.Color.G}, B:{ColorBrush.Color.B}";
         }
+        #endregion
     }
 }
