49f24b59a8127fe5cace250dbb5eb742a2e1bc08
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-10 21:09:42 -0500

    ado-192 Add docs to the ColorListSampleData class

--- ParticleMaker/DesignData/ColorListSampleData.cs
+++ ParticleMaker/DesignData/ColorListSampleData.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Media;
 
 namespace ParticleMaker.DesignData
 {
+    /// <summary>
+    /// Provides sample data for the use at design time for user controls.
+    /// </summary>
     public class ColorListSampleData
     {
+        #region Props
+        /// <summary>
+        /// A list of color data.
+        /// </summary>
         public ObservableCollection<ColorItem> Colors { get; set; }
 
+        /// <summary>
+        /// A color value.
+        /// </summary>
         public SolidColorBrush ColorValue { get; set; }
+        #endregion
     }
 }
