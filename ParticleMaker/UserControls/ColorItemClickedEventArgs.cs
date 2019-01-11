1f12002b7269d07c8a25bfba10e0fc13af1cfd35
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-10 20:04:10 -0500

    ado-193 Add documentation to ColorItemClickedEventArgs class

--- ParticleMaker/UserControls/ColorItemClickedEventArgs.cs
+++ ParticleMaker/UserControls/ColorItemClickedEventArgs.cs
@@ -3,10 +3,17 @@ using System.Windows.Media;
 
 namespace ParticleMaker.UserControls
 {
-    //TODO: Add docs to entire class
+    /// <summary>
+    /// Holds information about when a user clicks a <see cref="ColorItem"/> with the mouse.
+    /// </summary>
     public class ColorItemClickedEventArgs : EventArgs
     {
         #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="ColorItemClickedEventArgs"/>.
+        /// </summary>
+        /// <param name="id">The id of the <see cref="ColorItem"/> that was clicked.</param>
+        /// <param name="color">The color of the item that was clicked.</param>
         public ColorItemClickedEventArgs(int id, Color color)
         {
             Id = id;
@@ -16,8 +23,14 @@ namespace ParticleMaker.UserControls
 
 
         #region Props
+        /// <summary>
+        /// The ID of the <see cref="ColorItem"/>.
+        /// </summary>
         public int Id { get; private set; }
 
+        /// <summary>
+        /// The <see cref="Color"/> of the <see cref="ColorItem"/>.
+        /// </summary>
         public Color Color { get; private set; }
         #endregion
     }
