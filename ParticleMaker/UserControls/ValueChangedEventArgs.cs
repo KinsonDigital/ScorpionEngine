e54af7d34180d5fac284c3ad3f74994d22123767
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-10 20:29:59 -0500

    ado-205 Add docs to ValueChangedEventArgs class

--- ParticleMaker/UserControls/ValueChangedEventArgs.cs
+++ ParticleMaker/UserControls/ValueChangedEventArgs.cs
@@ -1,8 +1,20 @@
 ﻿namespace ParticleMaker.UserControls
 {
+    /// <summary>
+    /// Holds information about when a value has changed.
+    /// </summary>
     public class ValueChangedEventArgs
     {
+        #region Props
+        /// <summary>
+        /// Gets the old value before the change.
+        /// </summary>
         public float OldValue { get; internal set; }
+
+        /// <summary>
+        /// Gets the new value after the change.
+        /// </summary>
         public float NewValue { get; internal set; }
+        #endregion
     }
 }
