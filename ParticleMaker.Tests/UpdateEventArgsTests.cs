096fb071a9183c0c667dc992e447baf254d269ce
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-14 13:23:58 -0500

    tsk-250 - Create unit tests for UpdateEventArgs class

---
+++ ParticleMaker.Tests/UpdateEventArgsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using NUnit.Framework;
+using ParticleMaker.CustomEventArgs;
+using System;
+
+namespace ParticleMaker.Tests
+{
+    [TestFixture]
+    public class UpdateEventArgsTests
+    {
+        #region Constructor Tests
+        [Test]
+        public void Ctor_WhenInvoking_ProperlySetsGameTimeProp()
+        {
+            //Arrange
+            var gameTime = new GameTime() { ElapsedGameTime = new TimeSpan(11, 22, 33, 44, 55) };
+            var eventArgs = new UpdateEventArgs(gameTime);
+            var expected = new TimeSpan(11, 22, 33, 44, 55);
+
+            //Act
+            var actual = eventArgs.GameTime.ElapsedGameTime;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
