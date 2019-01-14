8bdee9466d75ec729ac45571ef3de0938c6b980e
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-14 10:55:43 -0500

    tsk-180 - Create unit tests for ColorItemClickedEventArgs

---
+++ ParticleMaker.Tests/ColorItemClickedEventArgsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ParticleMaker.UserControls;
+using System.Windows.Media;
+
+namespace ParticleMaker.Tests
+{
+    [TestFixture]
+    public class ColorItemClickedEventArgsTests
+    {
+        #region Constructor Tests
+        [Test]
+        public void Ctor_WhenInvoking_ProperlySetsProperties()
+        {
+            //Arrange
+            var eventArgs = new ColorItemClickedEventArgs(1234, Color.FromArgb(255, 11, 22, 33));
+            var expectedId = 1234;
+            var expectedColor = Color.FromArgb(255, 11, 22, 33);
+
+            //Act
+            var actualId = eventArgs.Id;
+            var actualColor = eventArgs.Color;
+
+            //Assert
+            Assert.AreEqual(expectedId, actualId);
+            Assert.AreEqual(expectedColor, actualColor);
+        }
+        #endregion
+    }
+}
