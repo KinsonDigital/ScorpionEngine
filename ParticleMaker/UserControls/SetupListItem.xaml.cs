6780706f42bef63b177da35b373e3819c2c32904
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-24 06:57:41 -0500

    tsk-290 - Create DP property for the setup path

--- ParticleMaker/UserControls/SetupListItem.xaml.cs
+++ ParticleMaker/UserControls/SetupListItem.xaml.cs
@@ -20,9 +20,36 @@ namespace ParticleMaker.UserControls
     /// </summary>
     public partial class SetupListItem : UserControl
     {
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="SetupListItem"/>.
+        /// </summary>
         public SetupListItem()
         {
             InitializeComponent();
         }
+        #endregion
+
+
+        #region Props
+        #region Dependency Props
+        /// <summary>
+        /// Registers the <see cref="SetupPath"/>.
+        /// </summary>
+        public static readonly DependencyProperty SetupPathProperty =
+            DependencyProperty.Register(nameof(SetupPath), typeof(string), typeof(SetupListItem), new PropertyMetadata(""));
+        #endregion
+
+
+        /// <summary>
+        /// Gets or sets the path to the setup file.
+        /// Must be a full path with file name.
+        /// </summary>
+        public string SetupPath
+        {
+            get { return (string)GetValue(SetupPathProperty); }
+            set { SetValue(SetupPathProperty, value); }
+        }
+        #endregion
     }
 }
