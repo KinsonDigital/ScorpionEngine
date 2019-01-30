331dcc05364826170953f238fd5db471728a773f
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-30 08:22:21 -0500

    tsk-340 - Create label

--- ParticleMaker/UserControls/ParticleListItem.xaml.cs
+++ ParticleMaker/UserControls/ParticleListItem.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ParticleMaker.UserControls
 {
@@ -14,6 +15,27 @@ namespace ParticleMaker.UserControls
         public ParticleListItem()
         {
             InitializeComponent();
+        }
+        #endregion
+
+
+        #region Props
+        #region Dependency Props
+        /// <summary>
+        /// Creates a new instance of <see cref="ParticleName"/>.
+        /// </summary>
+        protected static readonly DependencyProperty ParticleNameProperty =
+            DependencyProperty.Register(nameof(ParticleName), typeof(string), typeof(ParticleListItem), new PropertyMetadata(""));
+        #endregion
+
+
+        /// <summary>
+        /// Gets or sets the name of the particle.
+        /// </summary>
+        protected string ParticleName
+        {
+            get { return (string)GetValue(ParticleNameProperty); }
+            set { SetValue(ParticleNameProperty, value); }
         }
         #endregion
     }
