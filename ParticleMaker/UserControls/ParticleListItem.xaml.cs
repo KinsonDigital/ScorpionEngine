b684770aff0a0974054d1874ed585cf025966a29
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-30 08:15:48 -0500

    tsk-260 - Create user control

---
+++ ParticleMaker/UserControls/ParticleListItem.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace ParticleMaker.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ParticleListItem.xaml
+    /// </summary>
+    public partial class ParticleListItem : UserControl
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="ParticleListItem"/>.
+        /// </summary>
+        public ParticleListItem()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
