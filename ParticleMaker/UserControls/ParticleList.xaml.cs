eb973c689fe0231462d06bd7a39d23e624af2bf0
Calvin Wilkinson <kinsondigital@gmail.com>
2019-01-30 14:40:22 -0500

    tsk-329 - Create user control

---
+++ ParticleMaker/UserControls/ParticleList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParticleMaker.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ParticleList.xaml
+    /// </summary>
+    public partial class ParticleList : UserControl
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="ParticleList"/>.
+        /// </summary>
+        public ParticleList()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
