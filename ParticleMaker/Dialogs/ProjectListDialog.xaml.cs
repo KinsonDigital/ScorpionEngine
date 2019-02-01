a7058b92ce43367cc9a921aae6e10f099ee8f2e2
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-01 08:25:09 -0500

    tsk-280 - Create project list dialog window

---
+++ ParticleMaker/Dialogs/ProjectListDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ParticleMaker.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ProjectListDialog.xaml
+    /// </summary>
+    public partial class ProjectListDialog : Window
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="ProjectListDialog"/>.
+        /// </summary>
+        public ProjectListDialog()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
