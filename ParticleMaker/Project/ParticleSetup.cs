2e21a22a2b0158cc7ccea28bab3476a3fb0101ae
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-06 18:33:24 -0500

    tsk-440 - Create ParticleSetup class

--- ParticleMaker/Project/ParticleSetup.cs
+++ ParticleMaker/Project/ParticleSetup.cs
@@ -1,6 +1,119 @@
-﻿namespace ParticleMaker.Project
+﻿using System.Collections.ObjectModel;
+
+namespace ParticleMaker.Project
 {
-    class ParticleSetup
+    public class ParticleSetup
     {
+        #region Props
+        /// <summary>
+        /// Gets or sets the minimum value of the red color component range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int RedMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum value of the red color component range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int RedMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum value of the green color component range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int GreenMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum value of the green color component range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int GreenMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum value of the blue color component range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int BlueMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum value of the blue color component range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int BlueMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum size of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float SizeMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum size of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float SizeMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum angle of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float AngleMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum angle of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float AngleMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum angular velocity of the range that a <see cref="Particle"/> be will randomly set to.
+        /// </summary>
+        public float AngularVelocityMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum angular velocity of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float AngularVelocityMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum X velocity of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float VelocityXMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum X velocity of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float VelocityXMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum X velocity of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float VelocityYMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum Y velocity of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public float VelocityYMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum life time of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int LifetimeMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum life time of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int LifetimeMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the minimum spawn rate of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int SpawnRateMin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum spawn rate of the range that a <see cref="Particle"/> will be randomly set to.
+        /// </summary>
+        public int SpawnRateMax { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating if the colors will be randomly chosen from a list.
+        /// </summary>
+        public bool UseColorsFromList { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of colors to randomly choose from.
+        /// </summary>
+        public ObservableCollection<ColorItem> Colors { get; set; }
+        #endregion
     }
 }
