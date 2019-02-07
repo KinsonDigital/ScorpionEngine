d5d442da96f087f5e99b849be2b1659aea0add86
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-06 19:30:36 -0500

    tsk-437 - Add ProjectName prop to ProjectSettings class

--- ParticleMaker/Project/ProjectSettings.cs
+++ ParticleMaker/Project/ProjectSettings.cs
@@ -5,9 +5,16 @@
     /// </summary>
     public class ProjectSettings
     {
+        #region Props
         /// <summary>
         /// Holds all of the deployement settings for all of a project's setups.
         /// </summary>
         public DeploymentSetting[] SetupDeploySettings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the project.
+        /// </summary>
+        public object ProjectName { get; internal set; }
+        #endregion
     }
 }
