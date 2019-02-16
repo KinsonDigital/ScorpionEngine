d4fbf2bd1d4a3b6d3bfdf8ccbc3a5c6f0d399666
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-16 07:22:16 -0500

    tsk-476 - Create ILoggerService interface

---
+++ ParticleMaker/Services/ILoggerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ParticleMaker.Services
+{
+    /// <summary>
+    /// Provides logging capibility.
+    /// </summary>
+    interface ILoggerService
+    {
+        #region Methods
+        /// <summary>
+        /// Logs generatl information using the given <paramref name="data"/>.
+        /// </summary>
+        /// <param name="data">The data/info to log.</param>
+        /// <param name="dateTime">The date time stamp of when the log will occur.</param>
+        void Log(string data, DateTime dateTime);
+
+
+        /// <summary>
+        /// Logs an error using the given information.
+        /// </summary>
+        /// <param name="data">The data/info to log.</param>
+        /// <param name="dateTime">The date time stamp of when the error has occured.</param>
+        /// <param name="errorNumber">The error number associated with the error.</param>
+        void LogError(string data, DateTime dateTime, int errorNumber);
+        #endregion
+    }
+}
