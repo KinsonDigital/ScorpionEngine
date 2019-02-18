9ee9be49d4da0797a84b98aa17deb517bf148b11
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-18 15:41:23 -0500

    tsk-418 - Create user control

---
+++ ParticleMaker/UserControls/SetupDeployment.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace ParticleMaker.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SetupDeployment.xaml
+    /// </summary>
+    public partial class SetupDeployment : UserControl
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="SetupDeployment"/>.
+        /// </summary>
+        public SetupDeployment()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
