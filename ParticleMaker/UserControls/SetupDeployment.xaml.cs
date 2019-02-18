df1bb7bcf004278651e787bb5fa1d1ad9278f97b
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-18 16:04:32 -0500

    tsk-419 - Create deploy path label and DP property

--- ParticleMaker/UserControls/SetupDeployment.xaml.cs
+++ ParticleMaker/UserControls/SetupDeployment.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ParticleMaker.UserControls
 {
     /// <summary>
     /// Interaction logic for SetupDeployment.xaml
     /// </summary>
-    public partial class SetupDeployment : UserControl
+    [ExcludeFromCodeCoverage]
+    public partial class SetupDeployment : UserControl, IDisposable
     {
+        #region Fields
+        private Task _refreshTask;
+        private CancellationTokenSource _tokenSrc;
+        #endregion
+
+
         #region Constructors
         /// <summary>
         /// Creates a new instance of <see cref="SetupDeployment"/>.
@@ -14,6 +26,81 @@ namespace ParticleMaker.UserControls
         public SetupDeployment()
         {
             InitializeComponent();
+
+            Unloaded += SetupDeployment_Unloaded;
+
+            _refreshTask = new Task(Refresh);
+            _refreshTask.Start();
+        }
+        #endregion
+
+
+        #region Props
+        #region Dependency Props
+        /// <summary>
+        /// Registers the <see cref="DeploymentPath"/> property.
+        /// </summary>
+        public static readonly DependencyProperty DeploymentPathProperty =
+            DependencyProperty.Register(nameof(DeploymentPath), typeof(string), typeof(SetupDeployment), new PropertyMetadata("", DeploymentPathChanged));
+        #endregion
+
+
+        /// <summary>
+        /// Gets or sets the directory path to where a setup deployment will occur.
+        /// </summary>
+        public string DeploymentPath
+        {
+            get { return (string)GetValue(DeploymentPathProperty); }
+            set { SetValue(DeploymentPathProperty, value); }
+        }
+        #endregion
+
+
+        #region Public Methods
+        /// <summary>
+        /// Disposes of the refresh task.
+        /// </summary>
+        public void Dispose()
+        {
+            _tokenSrc.Dispose();
+            _refreshTask.Dispose();
+        }
+        #endregion
+
+
+        #region Private Methods
+        /// <summary>
+        /// Cancels the refresh task.
+        /// </summary>
+        private void SetupDeployment_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _tokenSrc.Cancel();
+        }
+
+
+        /// <summary>
+        /// Refreshes the UI.
+        /// </summary>
+        private static void DeploymentPathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = (SetupDeployment)d;
+
+            if (ctrl == null)
+                return;
+
+            ctrl.Refresh();
+        }
+
+
+        /// <summary>
+        /// Refreshes the UI at a set interval.
+        /// </summary>
+        private void Refresh()
+        {
+            while(!_tokenSrc.IsCancellationRequested)
+            {
+                _tokenSrc.Token.WaitHandle.WaitOne(3000);
+            }
         }
         #endregion
     }
