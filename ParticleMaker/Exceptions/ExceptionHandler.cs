9e9903dbf626cba8d17f205fb56bc9bd4626f28c
Calvin Wilkinson <kinsondigital@gmail.com>
2019-02-18 05:01:45 -0500

    tsk-489 - Create class file

---
+++ ParticleMaker/Exceptions/ExceptionHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleMaker.Exceptions
+{
+    /// <summary>
+    /// Handles exceptions that are thrown by displaying them to the user and 
+    /// logging them for later inspection.
+    /// </summary>
+    public static class ExceptionHandler
+    {
+        
+    }
+}
