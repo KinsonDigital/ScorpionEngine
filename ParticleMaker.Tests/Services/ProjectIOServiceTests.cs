5bbf992cf3e4be127591fe1c45094f131b21a71d
Calvin Wilkinson <kinsondigital@gmail.com>
2019-03-28 07:25:48 -0400

    Build unit tests for ProjectIOService
    asdf

---
+++ ParticleMaker.Tests/Services/ProjectIOServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using NUnit.Framework;
+using ParticleMaker.Services;
+
+namespace ParticleMaker.Tests.Services
+{
+    [TestFixture]
+    public class ProjectIOServiceTests
+    {
+        #region Method Tests
+        [Test]
+        public void CheckRootProjectsFolder_WhenInvokingWhenDirectoryExists_DoesNotCreateFolder()
+        {
+            //Arrange
+            var mockDirService = new Mock<IDirectoryService>();
+            mockDirService.Setup(m => m.Exists(It.IsAny<string>())).Returns(true);
+
+            var service = new ProjectIOService(mockDirService.Object, new Mock<IFileService>().Object);
+
+            //Act
+            service.CheckRootProjectsFolder();
+
+            //Assert
+            mockDirService.Verify(m => m.Create(It.IsAny<string>()), Times.Never());
+        }
+
+
+        [Test]
+        public void CheckRootProjectsFolder_WhenInvokingWhenDirectoryDoesNotExist_CreatesFolder()
+        {
+            //Arrange
+            var mockDirService = new Mock<IDirectoryService>();
+            mockDirService.Setup(m => m.Exists(It.IsAny<string>())).Returns(false);
+
+            var service = new ProjectIOService(mockDirService.Object, new Mock<IFileService>().Object);
+
+            //Act
+            service.CheckRootProjectsFolder();
+
+            //Assert
+            mockDirService.Verify(m => m.Create(It.IsAny<string>()), Times.Once());
+        }
+        #endregion
+    }
+}
