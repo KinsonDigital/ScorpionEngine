0a2b2a11a083a6f08b52f9c368346bebe28be917
Calvin Wilkinson <kinsondigital@gmail.com>
2019-03-28 05:53:15 -0400

    tsk-626 - Create ProjectIOService class

---
+++ ParticleMaker/Services/ProjectIOService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleMaker.Services
+{
+    /// <summary>
+    /// Provides various project related services such and project and
+    /// setup checks to checking for illegal characters for file and directory management.
+    /// </summary>
+    public class ProjectIOService
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of <see cref="ProjectIOService"/>.
+        /// </summary>
+        /// <param name="directoryService">The service used to manage directories.</param>
+        /// <param name="fileService">The service used to manage files.</param>
+        public ProjectIOService(IDirectoryService directoryService, IFileService fileService)
+        {
+
+        }
+        #endregion
+    }
+}
