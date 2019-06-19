b1cfdcbc308d79543985f6b3b7a29d625d9f649c
Calvin Wilkinson <kinsondigital@gmail.com>
2019-06-19 18:46:54 -0400

    tsk-795 - Refactor MovementByKeyboardBehaviorTests class to use XUnit

--- KD.Scorpion.Engine.Tests/Behaviors/MovementByKeyboardBehaviorTests.cs
+++ KD.Scorpion.Engine.Tests/Behaviors/MovementByKeyboardBehaviorTests.cs
@@ -1,174 +1,25 @@
-﻿using Moq;
-using NUnit.Framework;
+﻿using System;
+using Moq;
+using Xunit;
 using KDScorpionCore;
 using KDScorpionCore.Input;
 using KDScorpionCore.Plugins;
 using KDScorpionEngine.Behaviors;
 using KDScorpionEngine.Entities;
-using KDScorpionEngineTests.Fakes;
 using PluginSystem;
+using KDScorpionEngineTests.Fakes;
 
 namespace KDScorpionEngineTests.Behaviors
 {
-    [TestFixture]
-    public class MovementByKeyboardBehaviorTests
+    public class MovementByKeyboardBehaviorTests : IDisposable
     {
+        #region Private Fields
         private Mock<IKeyboard> _mockCoreKeyboard;
-        #region Constructor Tests
-        [Test]
-        public void Ctor_WhenInvoking_CreatesMoveRightBehavior()
-        {
-            //Arrange
-            SetKeyboardKey(KeyCodes.Right);
-            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
-            entity.Initialize();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
-            var expected = 10;
-
-            //Act
-            behavior.Update(new EngineTime());
-            var actual = entity.Position.X;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [Test]
-        public void Ctor_WhenInvoking_CreatesMoveLeftBehavior()
-        {
-            //Arrange
-            SetKeyboardKey(KeyCodes.Left);
-            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
-            entity.Initialize();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
-            var expected = -10;
-
-            //Act
-            behavior.Update(new EngineTime());
-            var actual = entity.Position.X;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [Test]
-        public void Ctor_WhenInvoking_CreatesMoveUpBehavior()
-        {
-            //Arrange
-            SetKeyboardKey(KeyCodes.Up);
-            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
-            entity.Initialize();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
-            var expected = -10;
-
-            //Act
-            behavior.Update(new EngineTime());
-            var actual = entity.Position.Y;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [Test]
-        public void Ctor_WhenInvoking_CreatesMoveDownBehavior()
-        {
-            //Arrange
-            SetKeyboardKey(KeyCodes.Down);
-            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
-            entity.Initialize();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
-            var expected = 10;
-
-            //Act
-            behavior.Update(new EngineTime());
-            var actual = entity.Position.Y;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
         #endregion
 
 
-        #region Prop Tests
-        [Test]
-        public void MoveUpKey_WhenGettingAndSettingValue_CorrectlySetsValue()
-        {
-            //Arrange
-            SetKeyboardKey(It.IsAny<KeyCodes>());
-            var mockEntity = new Mock<DynamicEntity>();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
-            var expected = KeyCodes.W;
-
-            //Act
-            behavior.MoveUpKey = KeyCodes.W;
-            var actual = behavior.MoveUpKey;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [Test]
-        public void MoveDownKey_WhenGettingAndSettingValue_CorrectlySetsValue()
-        {
-            //Arrange
-            SetKeyboardKey(It.IsAny<KeyCodes>());
-            var mockEntity = new Mock<DynamicEntity>();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
-            var expected = KeyCodes.S;
-
-            //Act
-            behavior.MoveDownKey = KeyCodes.S;
-            var actual = behavior.MoveDownKey;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [Test]
-        public void MoveLeftKey_WhenGettingAndSettingValue_CorrectlySetsValue()
-        {
-            //Arrange
-            SetKeyboardKey(It.IsAny<KeyCodes>());
-            var mockEntity = new Mock<DynamicEntity>();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
-            var expected = KeyCodes.S;
-
-            //Act
-            behavior.MoveLeftKey = KeyCodes.S;
-            var actual = behavior.MoveLeftKey;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [Test]
-        public void MoveRightKey_WhenGettingAndSettingValue_CorrectlySetsValue()
-        {
-            //Arrange
-            SetKeyboardKey(It.IsAny<KeyCodes>());
-            var mockEntity = new Mock<DynamicEntity>();
-            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
-            var expected = KeyCodes.S;
-
-            //Act
-            behavior.MoveRightKey = KeyCodes.S;
-            var actual = behavior.MoveRightKey;
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-        #endregion
-
-
-        #region Public Methods
-        [SetUp]
-        public void Setup()
+        #region Constructors
+        public MovementByKeyboardBehaviorTests()
         {
             var mockEnginePluginLibrary = new Mock<IPluginLibrary>();
             _mockCoreKeyboard = new Mock<IKeyboard>();
@@ -183,10 +34,163 @@ namespace KDScorpionEngineTests.Behaviors
             });
             Plugins.PhysicsPlugins = mockPhysicsPluginLibrary.Object;
         }
+        #endregion
 
 
-        [TearDown]
-        public void TearDown()
+        #region Constructor Tests
+        [Fact]
+        public void Ctor_WhenInvoking_CreatesMoveRightBehavior()
+        {
+            //Arrange
+            SetKeyboardKey(KeyCodes.Right);
+            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
+            entity.Initialize();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
+            var expected = 10;
+
+            //Act
+            behavior.Update(new EngineTime());
+            var actual = entity.Position.X;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Ctor_WhenInvoking_CreatesMoveLeftBehavior()
+        {
+            //Arrange
+            SetKeyboardKey(KeyCodes.Left);
+            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
+            entity.Initialize();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
+            var expected = -10;
+
+            //Act
+            behavior.Update(new EngineTime());
+            var actual = entity.Position.X;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Ctor_WhenInvoking_CreatesMoveUpBehavior()
+        {
+            //Arrange
+            SetKeyboardKey(KeyCodes.Up);
+            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
+            entity.Initialize();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
+            var expected = -10;
+
+            //Act
+            behavior.Update(new EngineTime());
+            var actual = entity.Position.Y;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void Ctor_WhenInvoking_CreatesMoveDownBehavior()
+        {
+            //Arrange
+            SetKeyboardKey(KeyCodes.Down);
+            var entity = new DynamicEntity(new Vector[0], Vector.Zero);
+            entity.Initialize();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(entity, 10f);
+            var expected = 10;
+
+            //Act
+            behavior.Update(new EngineTime());
+            var actual = entity.Position.Y;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+
+        #region Prop Tests
+        [Fact]
+        public void MoveUpKey_WhenGettingAndSettingValue_CorrectlySetsValue()
+        {
+            //Arrange
+            SetKeyboardKey(It.IsAny<KeyCodes>());
+            var mockEntity = new Mock<DynamicEntity>();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
+            var expected = KeyCodes.W;
+
+            //Act
+            behavior.MoveUpKey = KeyCodes.W;
+            var actual = behavior.MoveUpKey;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void MoveDownKey_WhenGettingAndSettingValue_CorrectlySetsValue()
+        {
+            //Arrange
+            SetKeyboardKey(It.IsAny<KeyCodes>());
+            var mockEntity = new Mock<DynamicEntity>();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
+            var expected = KeyCodes.S;
+
+            //Act
+            behavior.MoveDownKey = KeyCodes.S;
+            var actual = behavior.MoveDownKey;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void MoveLeftKey_WhenGettingAndSettingValue_CorrectlySetsValue()
+        {
+            //Arrange
+            SetKeyboardKey(It.IsAny<KeyCodes>());
+            var mockEntity = new Mock<DynamicEntity>();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
+            var expected = KeyCodes.S;
+
+            //Act
+            behavior.MoveLeftKey = KeyCodes.S;
+            var actual = behavior.MoveLeftKey;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void MoveRightKey_WhenGettingAndSettingValue_CorrectlySetsValue()
+        {
+            //Arrange
+            SetKeyboardKey(It.IsAny<KeyCodes>());
+            var mockEntity = new Mock<DynamicEntity>();
+            var behavior = new MovementByKeyboardBehavior<DynamicEntity>(mockEntity.Object, 1);
+            var expected = KeyCodes.S;
+
+            //Act
+            behavior.MoveRightKey = KeyCodes.S;
+            var actual = behavior.MoveRightKey;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+
+        #region Public Methods
+        public void Dispose()
         {
             Plugins.EnginePlugins = null;
             Plugins.PhysicsPlugins = null;
