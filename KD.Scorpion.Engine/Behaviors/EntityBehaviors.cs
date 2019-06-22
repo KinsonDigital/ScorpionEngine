8ea778071743c79a8f503508d10218896ac4f170
Calvin Wilkinson <kinsondigital@gmail.com>
2019-06-22 15:40:19 -0400

    Exclude code coverage for EntityBehaviors class and add code docs

--- KD.Scorpion.Engine/Behaviors/EntityBehaviors.cs
+++ KD.Scorpion.Engine/Behaviors/EntityBehaviors.cs
@@ -4,83 +4,114 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace KDScorpionEngine.Behaviors
 {
+    [ExcludeFromCodeCoverage]
     public class EntityBehaviors : IEnumerable, IList<IBehavior>
     {
+        #region Private Fields
         public List<IBehavior> _items = new List<IBehavior>();
+        #endregion
 
 
         #region Props
+        /// <summary>
+        /// Gets an <see cref="IBehavior"/> at a specified index.
+        /// </summary>
+        /// <param name="index">The index of of the item to retrieve.</param>
+        /// <returns></returns>
         public IBehavior this[int index]
         {
             get => _items[index];
             set => _items[index] = value;
         }
 
+        /// <summary>
+        /// Gets the number of <see cref="IBehavior"/>s in the collection of <see cref="IBehavior"/>s.
+        /// </summary>
         public int Count => _items.Count;
 
+        /// <summary>
+        /// Gets a value indicating whether the collection of <see cref="IBehavior"/>s is readonly.
+        /// </summary>
         public bool IsReadOnly { get; } = false;
         #endregion
 
 
         #region Public Methods
-        public void Add(IBehavior item)
-        {
-            _items.Add(item);
-        }
+        /// <summary>
+        /// Adds the given <paramref name="item"/> to the collection of <see cref="IBehavior"/>s.
+        /// </summary>
+        /// <param name="item">The behavior to add.</param>
+        public void Add(IBehavior item) => _items.Add(item);
 
 
-        public void Clear()
-        {
-            _items.Clear();
-        }
+        /// <summary>
+        /// Removes all of the <see cref="IBehavior"/>s from the collection of <see cref="IBehavior"/>s.
+        /// </summary>
+        public void Clear() => _items.Clear();
 
 
-        public bool Contains(IBehavior item)
-        {
-            return _items.Contains(item);
-        }
+        /// <summary>
+        /// Determines whether the collection of <see cref="IBehavior"/>s contains the the specific
+        /// <paramref name="item"/>.
+        /// </summary>
+        /// <param name="item">The item check for.</param>
+        /// <returns></returns>
+        public bool Contains(IBehavior item) => _items.Contains(item);
 
 
-        public void CopyTo(IBehavior[] array, int arrayIndex)
-        {
-            _items.CopyTo(array, arrayIndex);
-        }
+        /// <summary>
+        /// Copies the <see cref="IBehavior"/>s to the given collection of <paramref name="behaviors"/>
+        /// starting at the given <paramref name="index"/>.
+        /// </summary>
+        /// <param name="behaviors"></param>
+        /// <param name="index"></param>
+        public void CopyTo(IBehavior[] behaviors, int index) => _items.CopyTo(behaviors, index);
 
 
-        public IEnumerator GetEnumerator()
-        {
-            return _items.GetEnumerator();
-        }
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection of <see cref="IBehavior"/>s.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator() => _items.GetEnumerator();
 
 
-        public int IndexOf(IBehavior item)
-        {
-            return _items.IndexOf(item);
-        }
+        /// <summary>
+        /// Determines the index of the given <paramref name="item"/> in the collection of <see cref="IBehavior"/>s.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public int IndexOf(IBehavior item) => _items.IndexOf(item);
 
 
-        public void Insert(int index, IBehavior item)
-        {
-            _items.Insert(index, item);
-        }
+        /// <summary>
+        /// Inserts the given <paramref name="item"/> to the collection of <see cref="IBehavior"/>s at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of where the item should be inserted.</param>
+        /// <param name="item">The item to insert.</param>
+        public void Insert(int index, IBehavior item) => _items.Insert(index, item);
 
 
-        public bool Remove(IBehavior item)
-        {
-            return _items.Remove(item);
-        }
+        /// <summary>
+        /// Removes the first occurence of the given <paramref name="item"/> from
+        /// the collection of <see cref="IBehavior"/>s.
+        /// </summary>
+        /// <param name="item">The behavior to remove.</param>
+        /// <returns></returns>
+        public bool Remove(IBehavior item) => _items.Remove(item);
 
 
-        public void RemoveAt(int index)
-        {
-            _items.RemoveAt(index);
-        }
+        /// <summary>
+        /// Removes the <see cref="IBehavior"/> at the given <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The zero-based index of the behavior to remove.</param>
+        public void RemoveAt(int index) => _items.RemoveAt(index);
 
-        [ExcludeFromCodeCoverage]
-        IEnumerator<IBehavior> IEnumerable<IBehavior>.GetEnumerator()
-        {
-            return _items.GetEnumerator();
-        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection of <see cref="IBehavior"/>s.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator<IBehavior> IEnumerable<IBehavior>.GetEnumerator() => _items.GetEnumerator();
         #endregion
     }
 }
