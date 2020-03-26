21c15476904e92105737b7da2f435e92a8f89f9b
Calvin Wilkinson <kinsondigital@gmail.com>
2020-03-25 20:16:45 -0400

    tsk-1515 - Remove old PluginSystem project from solution

--- PluginSystem/ExposedInternals.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo(assemblyName: "ScorpionCoreTests", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo(assemblyName: "ScorpionEngineTests", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo(assemblyName: "ScorpionUITests", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo(assemblyName: "ScorpionEngine", AllInternalsVisible = true)]
-[assembly: InternalsVisibleTo(assemblyName: "PluginSystemTests", AllInternalsVisible = true)]
