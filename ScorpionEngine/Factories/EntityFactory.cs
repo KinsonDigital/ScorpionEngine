0c37616aa72f8485fa54060aa6d2d5fe67591639
Calvin Wilkinson <kinsondigital@gmail.com>
2021-02-17 07:00:49 -0500

    Improve EntityFactory and create unit tests

--- ScorpionEngine/Factories/EntityFactory.cs
+++ ScorpionEngine/Factories/EntityFactory.cs
@@ -1,35 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using KDScorpionEngine.Content;
-using KDScorpionEngine.Entities;
-using KDScorpionEngine.Graphics;
-using Raptor.Content;
-using Raptor.Factories;
-using Raptor.Graphics;
+﻿// <copyright file="EntityFactory.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
 
 namespace KDScorpionEngine.Factories
 {
+    using KDScorpionEngine.Entities;
+    using KDScorpionEngine.Graphics;
+    using Raptor.Content;
+
+    /// <summary>
+    /// Creates new instances of entities.
+    /// </summary>
     public static class EntityFactory
     {
-        private static IContentLoader contentLoader;
-
-        static EntityFactory()
+        /// <summary>
+        /// Creates a non animating entity with a texture atlas with the given <paramref name="atlasName"/>
+        /// and sub texture in the atlas with the given <paramref name="subTextureName"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to create.</typeparam>
+        /// <param name="atlasName">The name of the texture atlas that contains the image of the entity.</param>
+        /// <param name="subTextureName">The name of the sub texture in the atlas.</param>
+        /// <returns>A new entity of the type <typeparamref name="TEntity"/>.</returns>
+        public static IEntity CreateNonAnimatedFromTextureAtlas<TEntity>(string atlasName, string subTextureName)
+            where TEntity : Entity, new()
         {
-            contentLoader = ContentLoaderFactory.CreateContentLoader();
+            var newEntity = new TEntity();
+
+            newEntity.RenderSection.TextureName = atlasName;
+            newEntity.RenderSection.SubTextureName = subTextureName;
+            newEntity.RenderSection.IsAnimated = false;
+            newEntity.RenderSection.TypeOfTexture = TextureType.SubTexture;
+
+            return newEntity;
         }
 
-        public static IEntity CreateNonAnimatedFromTextureAtlas(string atlasName, string subTextureName)
+        /// <summary>
+        /// Creates a non animated entity with an entire texture that matches the name <paramref name="name"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to create.</typeparam>
+        /// <param name="name">The name fo the texture used for the entity.</param>
+        /// <returns>A new entity of the type <typeparamref name="TEntity"/>.</returns>
+        public static IEntity CreateNonAnimatedFromTexture<TEntity>(string name)
+            where TEntity : Entity, new()
         {
-            return new Entity(atlasName, subTextureName);
+            var newEntity = new TEntity();
+
+            newEntity.RenderSection.TextureName = name;
+            newEntity.RenderSection.SubTextureName = string.Empty;
+            newEntity.RenderSection.IsAnimated = false;
+            newEntity.RenderSection.TypeOfTexture = TextureType.WholeTexture;
+
+            return newEntity;
         }
 
-        public static Entity CreateNonAnimatedFromTexture(string name)
+        /// <summary>
+        /// Creates an animated entity with a texture atlas with the given <paramref name="atlasName"/>
+        /// and sub texture in the atlas with the given <paramref name="subTextureName"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to create.</typeparam>
+        /// <param name="atlasName">The name of the texture atlas that contains the image of the entity.</param>
+        /// <param name="subTextureName">The name of the sub texture in the atlas.</param>
+        /// <returns>A new entity of the type <typeparamref name="TEntity"/>.</returns>
+        public static IEntity CreateAnimated<TEntity>(string atlasName, string subTextureName)
+            where TEntity : Entity, new()
         {
-            return new Entity(name, contentLoader.Load<ITexture>(name));
+            var newEntity = new TEntity
+            {
+                Animator = new Animator()
+            };
+
+            newEntity.RenderSection.TextureName = atlasName;
+            newEntity.RenderSection.SubTextureName = subTextureName;
+            newEntity.RenderSection.IsAnimated = true;
+            newEntity.RenderSection.TypeOfTexture = TextureType.SubTexture;
+
+            return newEntity;
         }
 
-        public static Entity CreateAnimated(string atlasName, string subTextureName)
-            => new Entity(atlasName, subTextureName, new Animator());
+        /// <summary>
+        /// Creates an animated entity with a texture atlas with the given <paramref name="atlasName"/>
+        /// and sub texture in the atlas with the given <paramref name="subTextureName"/> and with animation
+        /// that is controlled with the given <paramref name="animator"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to create.</typeparam>
+        /// <param name="atlasName">The name of the texture atlas that contains the image of the entity.</param>
+        /// <param name="subTextureName">The name of the sub texture in the atlas.</param>
+        /// <param name="animator">The animator used to control the animation.</param>
+        /// <returns>A new entity of the type <typeparamref name="TEntity"/>.</returns>
+        public static IEntity CreateAnimated<TEntity>(string atlasName, string subTextureName, IAnimator animator)
+            where TEntity : Entity, new()
+        {
+            var newEntity = new TEntity
+            {
+                Animator = animator,
+            };
+
+            newEntity.RenderSection.TextureName = atlasName;
+            newEntity.RenderSection.SubTextureName = subTextureName;
+            newEntity.RenderSection.IsAnimated = true;
+            newEntity.RenderSection.TypeOfTexture = TextureType.SubTexture;
+
+            return newEntity;
+        }
     }
 }
---
+++ Testing/ScorpionEngineTests/Factories/EntityFactoryTests.cs
@@ -0,0 +1,97 @@
+﻿// <copyright file="EntityFactoryTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace KDScorpionEngineTests.Factories
+{
+    using KDScorpionEngine.Entities;
+    using KDScorpionEngine.Factories;
+    using KDScorpionEngine.Graphics;
+    using Moq;
+    using Raptor.Content;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the <see cref="EntityFactory"/> class.
+    /// </summary>
+    public class EntityFactoryTests
+    {
+        private const string TextureAtlasName = "test-atlas";
+        private const string WholeTextureName = "test-whole-texture";
+        private const string SubTextureName = "test-sub-texture";
+        private readonly Mock<IContentLoader> mockContentLoader;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityFactoryTests"/> class.
+        /// </summary>
+        public EntityFactoryTests()
+        {
+            this.mockContentLoader = new Mock<IContentLoader>();
+        }
+
+        #region Method Tests
+        [Fact]
+        public void CreateNonAnimatedFromTextureAtlas_WhenInvoked_CreatesEntityWithCorrectState()
+        {
+            // Act
+            var entity = EntityFactory.CreateNonAnimatedFromTextureAtlas<Entity>(TextureAtlasName, SubTextureName);
+
+            // Assert
+            Assert.Equal(TextureAtlasName, entity.RenderSection.TextureName);
+            Assert.Equal("test-sub-texture", entity.RenderSection.SubTextureName);
+            Assert.False(entity.RenderSection.IsAnimated);
+            Assert.Null(entity.AtlasData);
+            Assert.Null(entity.Animator);
+            Assert.Equal(TextureType.SubTexture, entity.RenderSection.TypeOfTexture);
+        }
+
+        [Fact]
+        public void CreateNonAnimatedFromTexture_WhenInvoked_CreatesEntityWithCorrectState()
+        {
+            // Act
+            var entity = EntityFactory.CreateNonAnimatedFromTexture<Entity>(WholeTextureName);
+
+            // Assert
+            Assert.Equal(WholeTextureName, entity.RenderSection.TextureName);
+            Assert.Equal(string.Empty, entity.RenderSection.SubTextureName);
+            Assert.False(entity.RenderSection.IsAnimated);
+            Assert.Null(entity.AtlasData);
+            Assert.Null(entity.Animator);
+            Assert.Equal(TextureType.WholeTexture, entity.RenderSection.TypeOfTexture);
+        }
+
+        [Fact]
+        public void CreateAnimated_WhenInvoked_CreatesEntityWithCorrectState()
+        {
+            // Act
+            var entity = EntityFactory.CreateAnimated<Entity>(TextureAtlasName, SubTextureName);
+
+            // Assert
+            Assert.Equal(TextureAtlasName, entity.RenderSection.TextureName);
+            Assert.Equal(SubTextureName, entity.RenderSection.SubTextureName);
+            Assert.True(entity.RenderSection.IsAnimated);
+            Assert.Null(entity.AtlasData);
+            Assert.NotNull(entity.Animator);
+            Assert.Equal(TextureType.SubTexture, entity.RenderSection.TypeOfTexture);
+        }
+
+        [Fact]
+        public void CreateAnimated_WhenInvokedWithAnimator_CreatesEntityWithCorrectState()
+        {
+            // Act
+            var mockAnimator = new Mock<IAnimator>();
+
+            var entity = EntityFactory.CreateAnimated<Entity>(TextureAtlasName, SubTextureName, mockAnimator.Object);
+
+            // Assert
+            Assert.Equal(TextureAtlasName, entity.RenderSection.TextureName);
+            Assert.Equal(SubTextureName, entity.RenderSection.SubTextureName);
+            Assert.True(entity.RenderSection.IsAnimated);
+            Assert.Null(entity.AtlasData);
+            Assert.NotNull(entity.Animator);
+            Assert.Same(entity.Animator, mockAnimator.Object);
+            Assert.Equal(TextureType.SubTexture, entity.RenderSection.TypeOfTexture);
+        }
+        #endregion
+    }
+}
